8f69803b562ef9ac51f46a6be6a1af80f4fb0996
kas1kas <kas1kas@hotmail.com>
2025-02-08 10:59:38 +0100

    sh

---
+++ wk-1-install-system.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+echo "Installing system packages"
+sudo apt-get install git python3-pip python3-scipy swig fonts-freefont-ttf libopenjp2-7
---
+++ wk-2-install-python.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+echo "Installing python packages"
+sudo pip3 install pytz astral feedparser netifaces monotonic flask-restx rpi-ws281x adafruit-blinka python-tsl2591 pillow svgwrite freetype-py coloredlogs
